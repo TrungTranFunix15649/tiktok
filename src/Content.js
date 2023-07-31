206117ed30dd62da08ecf4adfa18c0b6d5d3fa5c
Doan Nhat Trung Tran <trungtdnfx15649@funix.edu.vn>
2023-07-31 22:29:00 +1000

    tab change

--- src/Content.js
+++ src/Content.js
@@ -5,27 +5,41 @@ import { useEffect , useState} from "react"
 // -Goij call back moi khi compoent re-call
 // 2. useEffect(callbakc,[])
 // - Chi goi callback mot lan sau khi component mounted
-// 3. useEffect(callback,
+// 3. useEffect(callback,[dep]
+// - Callback se duoc goi lai moi khi dependency thay doi
 // All cases, callback luon duoc goi sau khi component mounted
+const  tabs = ['posts', 'comments','albums',]
+
 function Content () {
     const [title, setTitle] = useState('')
     const [posts, setPosts] = useState([])
+    const [type, setType] = useState('posts')
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
+        
+        fetch(`https://jsonplaceholder.typicode.com/${type}`)
         .then(res => res.json())
         .then(posts => {
             setPosts(posts)
         })
-    },[])
+    },[type])
     return (
         <div>
+            {tabs.map(tab=>(
+                <button 
+                    key={tab}
+                    style={type===tab? {
+                        color: '#fff',
+                        backgroundColor:'#333'
+                    }: {}}
+                    onClick={()=>setType(tab)}>{tab}</button>
+            ))}
             <input 
                 value ={title}
                 onChange ={e=>setTitle(e.target.value)}
             />
             <ul>
                 {posts.map(post => (
-                    <li key={post.id}>{post.title}</li>
+                    <li key={post.id}>{post.title || post.name}</li>
                 ))}
             </ul>
         </div>
