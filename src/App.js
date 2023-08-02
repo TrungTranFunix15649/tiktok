4778791a36b33ee4000fe6f848e97ea44470443b
Doan Nhat Trung Tran <trungtdnfx15649@funix.edu.vn>
2023-08-02 22:21:13 +1000

    Add product

--- src/App.js
+++ src/App.js
@@ -1,31 +1,52 @@
-import { useRef, useState, useEffect } from "react";
+import {useState, useMemo, useRef } from "react";
 import Content from "./Content";
 function App() {
  
-  const [count, setCount] = useState(60)
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+  const [products, setProducts] = useState([])
 
-    const timerId  = useRef()
-    const preCount = useRef()
-    useEffect(() =>{
-      preCount.current = count
-    }, [count])
+  const nameRef = useRef()
   
-    const handleStart = () => {
-        timerId.current = setInterval(() =>{
-            setCount(prevCount => prevCount -1)
-        },1000)
-        console.log('start->', timerId.current)
+    const handleSubmit =()=> {
+      setProducts([...products, {
+        name,
+        price: +price
+      }])
+      setName('')
+      setPrice('')
+
+      nameRef.current.focus()
     }
-    console.log(count, preCount.current)
-    const handleStop = () => {
-        clearInterval(timerId.current)
-        console.log('stop->', timerId.current)
-    }
+
+    const total = useMemo(()=> {
+      const result = products.reduce((result, prod)=> result+prod.price, 0)
+      console.log('Tinh toan lai')
+      return result
+    }, [products])
     return (
-        <div style={{padding:20}}>
-            <h1>{count}</h1>
-            <button onClick={handleStart}>Start</button>
-            <button onClick ={handleStop}>Stop</button>
+        <div style={{padding:"20px 10px"}}>
+          <input
+            ref={nameRef} 
+            value = {name}
+            placeholder="Enter name ..."
+            onChange = {e=> setName(e.target.value)}
+          />
+          <br/>
+          <input 
+            value = {price}
+            placeholder="Enter price ..."
+            onChange = {e=> setPrice(e.target.value)}
+          />
+          <br/>
+          <button onClick={handleSubmit}>Add</button>
+          <br/>
+          Total: {total}
+          <ul>
+            {products.map((product, index) =>(
+              <li key={index}>{product.name} - {product.price}</li>
+            ))}
+          </ul>
         </div>
     )
     // <div className="App" style={{padding:20}}>
--- src/Content.js
+++ src/Content.js
@@ -1,40 +1,13 @@
-import { useRef , useState} from "react"
+import { memo} from "react"
 
-// 1. useEffect(callback)
-// - Goi callbakc moi khi component render
-// -Goij call back moi khi compoent re-call
-// 2. useEffect(callbakc,[])
-// - Chi goi callback mot lan sau khi component mounted
-// 3. useEffect(callback,[dep]
-// - Callback se duoc goi lai moi khi dependency thay doi
-// 1. All cases, callback luon duoc goi sau khi component mounted
-// 2. Cleanup duoc goi truoc khi component unmounted
-// 3. Cleanup fuc luon duoc goi truoc khi callback duoc goi (tru lan mounted)
-
-
-function Content () {
-    const [count, setCount] = useState(60)
-
-    let timerId  = useRef()
-
-    const handleStart = () => {
-        timerId.current = setInterval(() =>{
-            setCount(preCount => preCount = count-1)
-        },1000)
-        console.log('start->', timerId.current)
-    }
-
-    const handleStop = () => {
-        clearInterval(timerId.current)
-        console.log('stop->', timerId.current)
-    }
+function Content ({onIncrease}) {
+    console.log("Rendering")
     return (
-        <div style={{padding:20}}>
-            <h1>{count}</h1>
-            <button onClick={handleStart}>Start</button>
-            <button onClick ={handleStop}>Stop</button>
-        </div>
+        <>
+        <h2>Hello Anh em F8 </h2>
+        <button onClick={onIncrease}>Click me!</button>
+        </>
     )
 }
 
-export default Content
+export default memo(Content)
