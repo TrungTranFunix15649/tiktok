15a01d09e20a1652a646ff3cced6a7a9f38f92c7
Doan Nhat Trung Tran <trungtdnfx15649@funix.edu.vn>
2023-08-03 20:48:59 +1000

    add logger

---
+++ src/Todo/logger.js
@@ -0,0 +1,14 @@
+function logger(reducer) {
+    return (prevState, action) => {
+       console.group(action.type)
+        console.log('prev state:', prevState)
+        console.log('action:', action)
+       const newState = reducer(prevState, action)
+        console.log('new state:', newState)
+       console.groupEnd()
+
+       return newState
+    }
+}
+
+export default logger;
