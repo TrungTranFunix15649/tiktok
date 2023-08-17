02e1ca82ddbeb549a47d5136024281727e9cc5bb
Doan Nhat Trung Tran <trungtdnfx15649@funix.edu.vn>
2023-08-17 23:33:18 +1000

    Search logic

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import classNames from 'classnames/bind';
+import Image from '~/components/Image';
 import styles from './AccountItem.module.scss';
 
 const cx = classNames.bind(styles);
@@ -8,9 +9,10 @@ const cx = classNames.bind(styles);
 function AccountItem() {
     return (
         <div className={cx('wrapper')}>
-            <img
+            <Image
                 className={cx('avatar')}
-                src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/afd239facd07b5c6c26792f07f885afa~c5_300x300.webp?x-expires=1691816400&x-signature=89jDKE70YurEJ%2BFwGcgFOV1St%2FY%3D"
+                // delete https: from src to create error image
+                src="//p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/afd239facd07b5c6c26792f07f885afa~c5_300x300.webp?x-expires=1691816400&x-signature=89jDKE70YurEJ%2BFwGcgFOV1St%2FY%3D"
                 alt="alison"
             />
             <div className={cx('info')}>
